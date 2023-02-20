b32774a21315e5b85debcb6c7705f2487cba521a
Dawn <121364556+DevilishDawn431@users.noreply.github.com>
2023-02-20 10:42:24 -0800

    Update installer.lua
    installer for pixel os

--- installer.lua
+++ installer.lua
@@ -1,1 +1,14 @@
-
+print("downloading basalt...")
+shell.run("wget", "run", "https://basalt.madefor.cc/install.lua source basalt dev")
+print("downloading pine3d...")
+shell.run("pastebin", "run", "qpJYiYs2")
+print("downloading updater...")
+shell.run(""wget", "run", "https://raw.githubusercontent.com/AngellusMortis/cc-updater/master/install.lua")
+print("fetching filesystem...")
+pirnt("fetching default apps...")
+print("downloading apis...")
+print("fetching network drivers...")
+print("downloading recent updates...")
+local noupdates
+  if noupdates true then
+    print("no updates, resuming installation process...")
